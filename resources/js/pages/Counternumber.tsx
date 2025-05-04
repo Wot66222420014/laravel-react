285e69ddcafc758f2fe4eaebb76439c1a49979a9
Wot66222420014 <151432867+Wot66222420014@users.noreply.github.com>
2025-05-04 12:43:44 +0700

    Initial commit

---
+++ resources/js/pages/Counternumber.tsx
@@ -0,0 +1,29 @@
+import BootstrapLayout from "@/Layouts/BootstrapLayout";
+import { Head } from "@inertiajs/react";
+import { useState } from "react";
+
+const Counternumber: React.FC = () => {
+    const [count, setCount] = useState<number>(0); // กำหนดประเภทของ state เป็น number
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <BootstrapLayout>
+            <Head title="Counter Number" />
+            <div className="container">
+                <h2>Counter Number</h2>
+                <p>Current Count: {count}</p>
+                <button onClick={increment} className="btn btn-success">Increment</button>
+                <button onClick={decrement} className="btn btn-danger">Decrement</button>
+            </div>
+        </BootstrapLayout>
+    );
+};
+
+export default Counternumber;
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
+
+Route::get('/test', function () {
+    return Inertia::render('Test');
+})->name('test');
+
+Route::get('/tictactoe', function () {
+    return Inertia::render('Tictactoe');
+})->name('tictactoe');
+
+
+Route::get('/hello-teacher', function () {
+    return Inertia::render('HelloTeacher');
+})->name('hello-teacher');
+
+
+Route::get('/about-page', function () {
+    return Inertia::render('AboutPage');
+})->name('about-page');
+
+Route::get('/home-page', function () {
+    return Inertia::render('HomePage');
+})->name('home-page');
+
+Route::get('/bootstrap', function () {
+    return Inertia::render('BootstrapContent');
+})->name('bootstrap');
+
+Route::get('/circle', function () {
+    return Inertia::render('Circle');
+})->name('circle');
+
+Route::get('/counter', function () {
+    return Inertia::render('Counter');
+})->name('counter');
+
+Route::get('/form-example', function () {
+    return Inertia::render('FormExample');
+})->name('form-example');
+
+Route::get('/list-manager', function () {
+    return Inertia::render('ListManager');
+})->name('list-manager');
+
+Route::get('/infinite-scroll', function () {
+    return Inertia::render('InfiniteScrollExample');
+})->name('infinite-scroll');
+
+
+
+Route::get('/Counter-number', function () {
+    return Inertia::render('Counternumber'); // ชื่อคอมโพเนนต์ต้องตรงกับชื่อไฟล์
+})->name('Counter-number');
